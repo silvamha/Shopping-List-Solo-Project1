296a4a0780976f7db5a5261b46fe749211b86079
Ed <silva.email@yahoo.com>
2024-03-22 16:45:50 -0500

    Refactor input and button event handlers in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
-<template>
+<template v-cloak>
   <h1>{{ header }}</h1>
-  <input type="text" placeholder="Add An Item" v-model="newItem" @keyup.enter="items.push(newItem)"/>
-  <button class="btn" @click="items.push(newItem)">Save Item</button>
+  <input type="text" placeholder="Add An Item" v-model="newItem" @keyup.enter="saveItem" />
+  <button class="btn" @click="saveItem">Save Item</button>
   <ul>
+    <p v-if="items.length === 0">Nice Job! You bought all you</p>
     <li v-for="item in items">{{ item }}</li>
   </ul>
 </template>
@@ -13,38 +14,52 @@ export default {
   data() {
     return {
       header: "Shopping List App",
-      newItem:[],
+      newItem: [],
       items: [
-        'Chicken',
-        'Rice',
-        'Sugar',
+        // 'Chicken',
+        // 'Rice',
+        // 'Sugar',
       ]
     }
   },
-  methods:{
-    addItems(){
-      
+  methods: {
+    saveItem() {
+      this.items.push(this.newItem)
+      this.newItem = ''
     }
   }
 }
 </script>
 
-<!-- <style>
+<style>
 body {
   background-color: lightgray;
 }
 
 input {
   width: 300px;
+  height: 30px;
 }
 
 #app {
   display: flex;
   flex-direction: column;
   justify-items: center;
-
   margin: auto;
   height: 600px;
   width: 600px;
 }
-</style> -->
+
+.btn {
+  height: 30px;
+  width: 300px;
+  margin-top: 5px;
+  border-radius: 5px;
+  background-color: blue;
+  color: white;
+}
+
+[v-cloak] {
+  display: none;
+}
+</style>
