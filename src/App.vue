340417dd6c13234ff71b0e38747acc6a3f68ba71
Ed <silva.email@yahoo.com>
2024-03-22 10:38:23 -0500

    Update App.vue and main.js

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
-<h1>Hello World</h1>
+<h1>Shopping List Application</h1>
 </template>
 
 <script>
+export default {
+  name: "App",
+  data() {
+    return {
+      msg: "This is only a test"
+    }
+  },
+}
+
+
 
 </script>
-
--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-let vm = createApp(App)
-
-vm.mount('#app')
+createApp(App).mount("#app");
