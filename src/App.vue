8b931152c3a10f403ace8a3fdc494d3badadae04
Ed <silva.email@yahoo.com>
2024-03-23 19:13:36 -0500

    Refactor App.vue and ShoppingList.vue components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,70 @@
 <template v-cloak>
-<shoppinglist></shoppinglist>
+<!-- <shoppinglist></shoppinglist> -->
+<div class="header">
+    <h1>{{ header.toLocaleUpperCase() }}</h1>
+    <button v-if="state === 'default'" @click="changeState('edit')">Add Item</button>
+    <button v-else @click="changeState('default')">Cancel</button>
+  </div>
+
+  <!-- Work on the logic below then implement it into the code -->
+  <div  class="add-item-form">
+    <!-- 
+      Work on implementing this code to hide the form when not used
+    *****  v-if="state === 'edit'" *****
+     -->
+
+    <input type="text" placeholder="Add an Item" v-model="newItem" @keyup.enter="saveItem" />
+    <button class="btn" :disabled="newItem.length === 0" @click="saveItem">Save Item</button>
+  </div>
+  <a v-bind:href="newItem" target="_blank">Dynamic Link</a>
+  <ul>
+    <p v-if="items.length === 0">Nice Job! You bought all you</p>
+    <li v-for="item in items" :class="[item.purchased? 'strikeout' : '']">{{ item.label }}</li>
+  </ul>
 </template>
 
 <script>
-import Shoppinglist from './components/ShoppingList.vue'
+// import Shoppinglist from './components/ShoppingList.vue'
 export default {
   name: "App",
-  components: {
-Shoppinglist
+//   components: {
+// Shoppinglist
+//   },
+data() {
+    return {
+      state: 'default',
+      header: "Shopping List App",
+      newItem: '',
+      items: [
+        {
+          label:'chicken',
+          purchased:'true'
+        },
+        {
+          label:'sugar',
+          purchased:'false'
+        },
+        {
+          label:'rice',
+          purchased:'true'
+        }
+      ]
+    }
   },
-
+  methods: {
+    saveItem() {
+      this.items.unshift({
+        label:this.newItem,
+        purchased:false,
+      },)
+      this.newItem = ''
+      console.log(this.items)
+    },
+    changeState() {
+      this.state = newState
+      this.newItem = ''
+    }
+  }
 }
 </script>
 
@@ -61,6 +116,11 @@ li {
   list-style: none;
   font-size: 1.5em;
 }
+
+.strikeout{
+  text-decoration: line-through;
+}
+
 
 [v-cloak] {
   display: none;
--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -1,46 +1,12 @@
 <template>
-<div class="header">
-    <h1>{{ header.toLocaleUpperCase() }}</h1>
-    <button v-if="state === 'default'" @click="changeState('edit')">Add Item</button>
-    <button @click="changeState('default')">Cancel</button>
-  </div>
-
-  <div if="state === 'edit'" class="add-item-form">
-    <input type="text" placeholder="Add an Item" v-model="newItem" @keyup.enter="saveItem" />
-    <button class="btn" :disabled ="newItem.length === 0" @click="saveItem">Save Item</button>
-  </div>
-  <ul>
-    <p v-if="items.length === 0">Nice Job! You bought all you</p>
-    <li v-for="item in items">{{ item}}</li>
-  </ul>
+  
 </template>
 
 <script>
-export default{
-    name:"ShoppingList",
-    data() {
-    return {
-      state: 'default',
-      header: "Shopping List App",
-      newItem: '',
-      items: [
-        'Chicken',
-        'Rice',
-        'Sugar',
-      ]
-    }
-  },
-  methods: {
-    saveItem() {
-      this.items.unshift(this.newItem)
-      this.newItem = ''
-    },
-    changeState() {
-      this.state = newState
-      this.newItem = ''
-    }
-  }
-}
+// export default {
+//   name: "ShoppingList",
+  
+// }
 
 
 </script>
